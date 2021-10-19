cb28c0315e32c7df28b1c1ce872459029f74e03b
Ravi Majithia <ravikiran.majithia@joubel.com>
2021-10-19 11:36:20 +0200

    JI-2863 Show/hide submit button based on scoring and extra report is active

--- src/scripts/summary.js
+++ src/scripts/summary.js
@@ -316,7 +316,8 @@ class Summary extends H5P.EventDispatcher {
     const wrapper = document.createElement("div");
     wrapper.classList.add('h5p-interactive-book-summary-buttons');
 
-    if (H5PIntegration.reportingIsEnabled) {
+    if (H5P.scoringFlag === undefined || H5P.isExtraReportingActive === undefined
+      || (H5P.scoringFlag && H5P.isExtraReportingActive)) {
       const submitButton = this.addButton('icon-paper-pencil', this.l10n.submitReport);
       submitButton.classList.add('h5p-interactive-book-summary-submit');
       submitButton.onclick = () => {
