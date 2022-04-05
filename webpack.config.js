4f09da2daf4b234b2d602f9ebc361619adba2eb4
Paal Joergensen <pal.jorgensen@gmail.com>
2022-04-05 13:40:27 +0200

    Update node modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,72 @@
-var path = require('path');
-var nodeEnv = process.env.NODE_ENV || 'development';
-var isDev = (nodeEnv !== 'production');
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 
-var config = {
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProd = (nodeEnv === 'production');
+
+module.exports = {
   mode: nodeEnv,
+  optimization: {
+    minimize: isProd,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress:{
+            drop_console: true,
+          }
+        }
+      }),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'h5p-interactive-book.css'
+    })
+  ],
   entry: {
     dist: './src/entries/main.js'
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'h5p-interactive-book.js'
+    filename: 'h5p-interactive-book.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
         loader: 'babel-loader'
       },
       {
-        test:/\.scss$/,
-        include: path.resolve(__dirname, 'src/styles'),
-        use:['style-loader','css-loader', 'sass-loader']
+        test: /\.(s[ac]ss|css)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ''
+            }
+          },
+          { loader: "css-loader" },
+          {
+            loader: "sass-loader"
+          }
+        ]
       },
       {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'url-loader?limit=100000'
+        test: /\.svg|\.eot|\.woff2|\.woff|\.ttf$/,
+        include: path.join(__dirname, 'src/fonts'),
+        type: 'asset/resource'
       }
     ]
   },
+  stats: {
+    colors: true,
+    children: true,
+    errorDetails: true
+  },
   externals: {
     jquery: 'H5P.jQuery'
-  }
+  },
+  devtool: (isProd) ? undefined : 'eval-cheap-module-source-map'
 };
-
-if (isDev) {
-  config.devtool = 'inline-source-map';
-}
-
-module.exports = config;
