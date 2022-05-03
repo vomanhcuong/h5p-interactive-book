05096cebae413877964193db4572bcc052814181
Oliver Tacke <o.tacke@posteo.de>
2022-05-03 14:30:11 +0200

    Use CSS custom properties for custom colors

--- src/scripts/colors.js
+++ src/scripts/colors.js
@@ -1,22 +1,5 @@
 import Color from 'color';
-
-import ColorsCore from './colorOverrides/core.js';
-import ColorsJoubelUI from './colorOverrides/joubel-ui.js';
-
-import ColorsAudio from './colorOverrides/audio.js';
-import ColorsCoursePresentation from './colorOverrides/course-presentation.js';
-import ColorsDocumentationTool from './colorOverrides/documentation-tool.js';
-import ColorsFreeTextQuestion from './colorOverrides/free-text-question.js';
-import ColorsGuessTheAnswer from './colorOverrides/guess-the-answer.js';
-import ColorsInteractiveVideo from './colorOverrides/interactive-video.js';
-import ColorsMemoryGame from './colorOverrides/memory-game.js';
-import ColorsOpenEndedQuestion from './colorOverrides/open-ended-question.js';
-import ColorsQuestion from './colorOverrides/question.js';
-import ColorsQuestionnaire from './colorOverrides/questionnaire.js';
-import ColorsQuestionSet from './colorOverrides/question-set.js';
-import ColorsSimpleMultipleChoice from './colorOverrides/simple-multiple-choice.js';
-import ColorsSingleChoiceSet from './colorOverrides/single-choice-set.js';
-import ColorsTimeline from './colorOverrides/timeline.js';
+import './../styles/_color_overrides.scss';
 
 /**
  * Color class.
@@ -137,80 +120,21 @@ export default class Colors {
    * @return {string} CSS overrides.
    */
   static getCSS() {
-    let css = [];
-
-    // Cover
-    css.push(`.h5p-interactive-book-cover-bar { background-color: ${Colors.getColor(Colors.colorBase, { opacity: .05 })}; }`);
-    css.push(`.h5p-interactive-book-cover-readbutton button { background-color: ${Colors.colorBase}; }`);
-    css.push(`.h5p-interactive-book-cover-readbutton button:hover { background-color: ${Colors.getColor(Colors.colorBase, { opacity: .90 })}; }`);
-    css.push(`.h5p-interactive-book-cover-readbutton button:active { background-color: ${Colors.getColor(Colors.colorBase, { opacity: .95 })}; }`);
-    css.push(`.h5p-interactive-book-cover-readbutton button { color: ${Colors.colorText} }`);
-
-    // Navigation bar
-    css.push(`.h5p-interactive-book-navigation-section:hover { color: ${Colors.colorBase}; }`);
-    css.push(`.h5p-interactive-book-navigation-sectionlist :hover { color: ${Colors.colorBase}; }`);
-    css.push(`.h5p-interactive-book-navigation-chapter-button:not(.h5p-interactive-book-summary-menu-button) { background-color: ${Colors.getColor(Colors.colorBase, { opacity: .1 })}; }`);
-    css.push(`.h5p-interactive-book-navigation-chapter-button:not(.h5p-interactive-book-summary-menu-button).h5p-interactive-book-navigation-current { background-color: ${Colors.getColor(Colors.colorBase, { opacity: .1 })}; }`);
-    css.push(`.h5p-interactive-book-navigation-chapter-button:not(.h5p-interactive-book-summary-menu-button):hover { background-color: ${Colors.getColor(Colors.colorBase, { opacity: .05 })}; }`);
-    css.push(`.h5p-interactive-book-navigation-chapter-button:not(.h5p-interactive-book-summary-menu-button):active { background-color: ${Colors.getColor(Colors.colorBase, { opacity: .1 })}; }`);
-    css.push(`.h5p-interactive-book-navigation-chapter-button:not(.h5p-interactive-book-summary-menu-button) .h5p-interactive-book-navigation-chapter-title-text { color: ${Colors.colorBase}; }`);
-    css.push(`.h5p-interactive-book-navigation-maintitle { background-color: ${Colors.colorBase}; }`);
-    css.push(`.h5p-interactive-book-navigation-maintitle .navigation-title { color: ${Colors.colorText}; }`);
-
-    // Status bar
-    css.push(`.h5p-interactive-book-status { border-color: ${Colors.colorBase}; }`);
-    css.push(`.h5p-interactive-book-status-button:hover { background-color: ${Colors.getColor(Colors.colorBase, { opacity: .1 })}; }`);
-    css.push(`.h5p-interactive-book-status-button:active { background-color: ${Colors.getColor(Colors.colorBase, { opacity: .2 })}; }`);
-    css.push(`.h5p-interactive-book-status-button[disabled]:active { background-color: transparent; }`);
-    css.push(`.h5p-interactive-book-status-fullscreen { background-color: ${Colors.getColor(Colors.colorBase, { opacity: .05 })}; }`);
-    css.push(`.h5p-interactive-book-status-fullscreen.h5p-interactive-book-enter-fullscreen::before { color: ${Colors.colorBase}; }`);
-    css.push(`.h5p-interactive-book-status-fullscreen.h5p-interactive-book-exit-fullscreen::before { color: ${Colors.colorBase}; }`);
-    css.push(`.h5p-interactive-book-status-menu .icon-menu { color: ${Colors.colorBase}; }`);
-    css.push(`.h5p-interactive-book-status-menu.h5p-interactive-book-status-menu-active { background-color: ${Colors.colorBase}; }`);
-    css.push(`.h5p-interactive-book-status-menu.h5p-interactive-book-status-menu-active .icon-menu { color: ${Colors.colorText}; }`);
-    css.push(`.h5p-interactive-book-status-menu.h5p-interactive-book-status-menu-active:focus-visible { outline-color: ${Colors.colorBase}; }`);
-    css.push(`.h5p-interactive-book-status-menu.h5p-interactive-book-status-menu-active:hover { background-color: ${Colors.getColor(Colors.colorBase, { opacity: .9 })}; }`);
-    css.push(`.h5p-interactive-book-status-menu.h5p-interactive-book-status-menu-active:active { background-color: ${Colors.getColor(Colors.colorBase, { opacity: .95 })}; }`);
-    css.push(`.h5p-interactive-book-status-progress-number { color: ${Colors.colorBase}; text-decoration: none solid ${Colors.colorBase}; }`);
-    css.push(`.h5p-interactive-book-status-progressbar-back { background-color: ${Colors.getColor(Colors.colorBase, { opacity: .2 })}; }`);
-    css.push(`.h5p-interactive-book-status-progressbar-front { background-color: ${Colors.colorBase}; }`);
-    css.push(`.h5p-interactive-book-status-arrow { background-color: ${Colors.getColor(Colors.colorBase, { opacity: .05 })}; }`);
-    css.push(`.h5p-interactive-book-status-arrow .navigation-button { color: ${Colors.colorBase}; }`);
-
-    // Summary
-    const contrastColorProgress = Colors.computeContrastColor(Colors.colorBase, Colors.DEFAULT_COLOR_BG);
-    css.push(`.h5p-interactive-book-summary-page .h5p-interactive-box-summary-progress .h5p-interactive-book-summary-progressbox-bigtext { color: ${contrastColorProgress}; }`);
-    css.push(`.h5p-interactive-book-summary-page .h5p-interactive-book-summary-section-icon { color: ${contrastColorProgress}; }`);
-    css.push(`.h5p-interactive-book-summary-page .h5p-interactive-book-summary-overview-section h4 span[class^=icon-] { color: ${contrastColorProgress}; }`);
-    css.push(`.h5p-interactive-book-summary-page .h5p-interactive-book-summary-overview-section h4:hover { color: ${contrastColorProgress}; }`);
-
-    // General overrides
-    css = [...css, ColorsJoubelUI.getCSS(), ColorsCore.getCSS()];
-
-    // Content type based overrides
-    for (let machineName in Colors.COLOR_OVERRIDES) {
-      css = [...css, Colors.getContentTypeCSS(machineName)];
-    }
-
-    return css.join('');
+    return `:root{
+      --color-base: ${Colors.colorBase};
+      --color-base-5: ${Colors.getColor(Colors.colorBase, { opacity: .05 })};
+      --color-base-10: ${Colors.getColor(Colors.colorBase, { opacity: .1 })};
+      --color-base-20: ${Colors.getColor(Colors.colorBase, { opacity: .2 })};
+      --color-base-75: ${Colors.getColor(Colors.colorBase, { opacity: .75 })};
+      --color-base-80: ${Colors.getColor(Colors.colorBase, { opacity: .80 })};
+      --color-base-85: ${Colors.getColor(Colors.colorBase, { opacity: .85 })};
+      --color-base-90: ${Colors.getColor(Colors.colorBase, { opacity: .9 })};
+      --color-base-95: ${Colors.getColor(Colors.colorBase, { opacity: .95 })};
+      --color-text: ${Colors.colorText};
+      --color-contrast: ${Colors.computeContrastColor(Colors.colorBase, Colors.DEFAULT_COLOR_BG)};
+    }`;
   }
 }
-
-/** @const {object} Custom CSS overrides */
-Colors.COLOR_OVERRIDES = {};
-Colors.COLOR_OVERRIDES['H5P.Audio'] = ColorsAudio;
-Colors.COLOR_OVERRIDES['H5P.CoursePresentation'] = ColorsCoursePresentation;
-Colors.COLOR_OVERRIDES['H5P.DocumentationTool'] = ColorsDocumentationTool;
-Colors.COLOR_OVERRIDES['H5P.FreeTextQuestion'] = ColorsFreeTextQuestion;
-Colors.COLOR_OVERRIDES['H5P.GuessTheAnswer'] = ColorsGuessTheAnswer;
-Colors.COLOR_OVERRIDES['H5P.InteractiveVideo'] = ColorsInteractiveVideo;
-Colors.COLOR_OVERRIDES['H5P.MemoryGame'] = ColorsMemoryGame;
-Colors.COLOR_OVERRIDES['H5P.OpenEndedQuestion'] = ColorsOpenEndedQuestion;
-Colors.COLOR_OVERRIDES['H5P.Question'] = ColorsQuestion;
-Colors.COLOR_OVERRIDES['H5P.QuestionSet'] = ColorsQuestionSet;
-Colors.COLOR_OVERRIDES['H5P.SimpleMultipleChoice'] = ColorsSimpleMultipleChoice;
-Colors.COLOR_OVERRIDES['H5P.SingleChoiceSet'] = ColorsSingleChoiceSet;
-Colors.COLOR_OVERRIDES['H5P.Timeline'] = ColorsTimeline;
 
 /** @const {string} Preferred default color as defined in SCSS */
 Colors.DEFAULT_COLOR_BASE = Color('#1768c4');
